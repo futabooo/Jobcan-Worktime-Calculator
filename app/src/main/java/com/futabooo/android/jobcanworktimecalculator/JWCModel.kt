904284c3a274248df1fecc9ae2b335b5333a111b
futabooo <takahiro.futagawa@gmail.com>
2018-08-28 00:44:55 +0900

    Modify codestyle

--- app/src/main/java/com/futabooo/android/jobcanworktimecalculator/JWCModel.kt
+++ app/src/main/java/com/futabooo/android/jobcanworktimecalculator/JWCModel.kt
@@ -4,22 +4,33 @@ import org.jsoup.nodes.Document
 
 class Attendance constructor(private val document: Document) {
 
-  val date: String = document.select("table.left:contains(ユーザー情報) tbody tr:eq(0) td").text()
-  val requiredWorkDay: String = document.select("table.left:contains(ユーザー情報) tbody tr:eq(3) td").text().removeSuffix("日").trim()
-  val actualWorkDay: String = document.select("table.right:contains(基本項目) tbody tr:eq(0) td").text()
-  val actualWorkTime: String = document.select("table.left:contains(労働時間) tbody tr:eq(0) td").text().replace(":", ".")
+  val date: String = document.select("table.left:contains(ユーザー情報) tbody tr:eq(0) td")
+      .text()
+  val requiredWorkDay: String = document.select("table.left:contains(ユーザー情報) tbody tr:eq(3) td")
+      .text()
+      .removeSuffix("日")
+      .trim()
+  val actualWorkDay: String = document.select("table.right:contains(基本項目) tbody tr:eq(0) td")
+      .text()
+  val actualWorkTime: String = document.select("table.left:contains(労働時間) tbody tr:eq(0) td")
+      .text()
+      .replace(":", ".")
   val holiday: String = document.select("table.right:contains(消化した休暇) tbody:eq(3) td")
-      .map { it.text().toFloat() }
+      .map {
+        it.text()
+            .toFloat()
+      }
       .sum()
       .toString()
 
-  fun requiredWorkTime():String{
+  fun requiredWorkTime(): String {
     return String.format("%1.2f", requiredWorkDay.toInt() * 8.0f)
   }
 
   fun savingTime(): String {
     val daysLeft = requiredWorkDay.toInt() - actualWorkDay.toInt()
-    val actual = actualWorkTime.replace(":", ".").toFloat()
+    val actual = actualWorkTime.replace(":", ".")
+        .toFloat()
     val savingTime = actual - (requiredWorkTime().toFloat() - (daysLeft * 8))
     return String.format("%1.2f", savingTime)
   }
