021a6c9be565b9857355d5b63784b09e2d3eb40c
futabooo <takahiro.futagawa@gmail.com>
2019-01-04 22:43:22 +0900

    Remove crashlytics

--- app/src/main/java/com/futabooo/android/jobcanworktimecalculator/JWC.kt
+++ app/src/main/java/com/futabooo/android/jobcanworktimecalculator/JWC.kt
@@ -1,23 +1,10 @@
 package com.futabooo.android.jobcanworktimecalculator
 
 import android.app.Application
-import com.crashlytics.android.Crashlytics
-import io.fabric.sdk.android.Fabric
-import timber.log.Timber
 
 class JWC : Application() {
 
   override fun onCreate() {
     super.onCreate()
-
-    if (BuildConfig.USE_CRASHLYTICS) {
-      Fabric.with(this, Crashlytics())
-    }
-
-    if (BuildConfig.DEBUG) {
-      Timber.plant(Timber.DebugTree())
-    } else {
-      Timber.plant(CrashReportingTree())
-    }
   }
 }
